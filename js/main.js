5d676da764239debe16c0948bc637b91e4c91780
Eiji Yoshida <yoshida@careerindex.co.jp>
2020-02-18 15:20:39 +0900

    検索結果ページ完成

--- js/main.js
+++ js/main.js
@@ -4,3 +4,15 @@ $(function() {
         $(this).next().slideToggle();
     });
 });
+
+// SP検索結果ページの条件選択タブ
+$(function() {
+    $(".search_result #search_tabs > li").on("click", function() {
+        var id = $(this).attr('id');
+        var targetId = "#result--" + id;
+        $(".search_result #search_tabs > li").removeClass("active");
+        $(this).addClass("active");
+        $(".search_result .search_buttons__condition_list").removeClass("open");
+        $(".search_result " + targetId).addClass("open");
+    });
+});
