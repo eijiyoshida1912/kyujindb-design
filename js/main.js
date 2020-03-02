d707bec3aa8e8c7fda1ce6c52ce7be30188ec139
Eiji Yoshida <yoshida@careerindex.co.jp>
2020-03-02 13:29:38 +0900

    企業詳細ページ完了

--- js/main.js
+++ js/main.js
@@ -16,3 +16,43 @@ $(function() {
         $(".search_result " + targetId).addClass("open");
     });
 });
+
+// 企業詳細ページのページ選択タブ
+$(function() {
+    $(".detail .tabs__list > li").on("click", function() {
+        var id = $(this).attr('id');
+        var targetId = "#" + id + "_tab_page";
+        console.log(targetId);
+        $(".detail .tabs__list > li").removeClass("tabs__this");
+        $(this).addClass("tabs__this");
+        $(".detail .tab_pages").removeClass("open");
+        $(".detail " + targetId).addClass("open");
+    });
+});
+
+// 企業詳細ページ内「カテゴリから・回答者から」切り替えタブ
+$(function() {
+    $(".detail .kuchikomi_top__tabs > li").on("click", function() {
+        var id = $(this).attr('id');
+        var targetId = "#" + id + "_page";
+        console.log(targetId);
+        $(".detail .kuchikomi_top__tabs > li").removeClass("active");
+        $(this).addClass("active");
+        $(".detail .kuchikomi_top_pages").removeClass("open");
+        $(".detail " + targetId).addClass("open");
+    });
+});
+
+// 企業詳細ページの報告するモーダル
+$(function() {
+    $(".detail .report > a").on("click", function() {
+        $(".report_modal").fadeIn();
+        $("body").addClass("modal_open");
+        return false;
+    });
+    $(".report_modal__cancel").on("click", function() {
+        $(".report_modal").fadeOut();
+        $("body").removeClass("modal_open");
+        return false;
+    });
+});
